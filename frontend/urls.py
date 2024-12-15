45ac3a9a97790e9317a27917b0e4ccdfa183a995
Jakub Gozdek <gj50974@zut.edu.pl>
2024-12-15 19:28:40 +0100

    Introduced basic form view that passes the data onto a seperate view via saving it in the session.

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,4 +3,6 @@ from frontend import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("your-name/", views.get_name, name = 'get_name'),
+    path("thanks/", views.result_page, name = 'result_page'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .forms import NameForm
 
 
 def home(request):
     print(request.build_absolute_uri()) #optional
-    return render(
-        request,
-        'home.html'
-    )
+    return render(request,'home.html')
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            request.session['form_data'] = form.cleaned_data    # Store data in the session
+            return HttpResponseRedirect(reverse('result_page')) # Pass data to the result page
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+    return render(request, "name.html", {"form": form})
+
+def result_page(request):
+    print(request.build_absolute_uri()) #optional
+    form = request.session.pop('form_data', None)
+    return render(request,'thanks.html', {"form": form})
