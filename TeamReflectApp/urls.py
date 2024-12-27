868a288174012a68cf9986e4d7f5ad25ea0d0684
Wojciech Gołubiński <gw50973@zut.edu.pl>
2024-12-27 16:24:20 +0100

    Dodane usuwanie użytkowników i profil admina ogólnego(jeszcze bez przypisania do grupy. login: admin hasło: 12345678 )

--- TeamReflectApp/urls.py
+++ TeamReflectApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from TeamReflectApp import views
+from . import views
 from django.views.generic.base import TemplateView
 from .views import SignUpView
+from .views import DeleteUserView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
@@ -10,4 +11,7 @@ urlpatterns = [
     path("your-feedback/", views.get_feedback, name = 'get_feedback'),
     path("feedbacks/", views.result_feedbacks, name = 'result_feedbacks'),
     path("signup/", SignUpView.as_view(), name="signup"),
+    path('delete_user/<int:user_id>/', DeleteUserView.as_view(), name='delete_user'),
+    path('users/', views.user_list, name='user_list'),
+    path('confirm_delete_user/<int:user_id>/', views.ConfirmDeleteUserView.as_view(), name='confirm_delete_user'),  # URL for user deletion confirmation
 ]
--- TeamReflectApp/views.py
+++ TeamReflectApp/views.py
@@ -6,6 +6,12 @@ from .models import Person, Feedback
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views import View
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 
 def home(request):
@@ -59,6 +65,42 @@ def result_feedbacks(request):
     #   OR
     feedbacks = Feedback.objects.all() # Fetch all Person records or the last inserted one
     return render(request, 'feedback_list.html', {"feedbacks": feedbacks})
+
+#@login_required
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'user_list.html',{'users': users})
+
+class DeleteUserView(View):
+    def get(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        
+        if user != request.user and not request.user.is_superuser:
+            raise Http404("You are not authorized to delete this user")
+        
+        user.delete()
+        
+        return redirect('home')
+
+
+class ConfirmDeleteUserView(View):
+    def get(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        if user != request.user and not request.user.is_superuser:   
+            return HttpResponseForbidden("You are not allowed to delete this user.")
+        
+        return render(request, 'confirm_delete.html', {'user': user})
+    
+    def post(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+         
+        if user != request.user and not request.user.is_superuser:
+            return HttpResponseForbidden("You are not allowed to delete this user.")
+        
+        user.delete()
+        return redirect('user_list')
+    
+
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
